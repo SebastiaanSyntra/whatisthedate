06bc5a154b0a7feee2f6978095c991bccdf568b9
Tom De Backer <tom.debacker@vdab.be>
2022-02-02 20:04:47 +0100

    [part 1] spring boot application

---
+++ src/main/java/com/realdolmen/devops/whatisthedate/Application.java
@@ -0,0 +1,12 @@
+package com.realdolmen.devops.whatisthedate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
