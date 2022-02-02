f0563311841844264c3527a2d713a3de365dcdd6
Tom De Backer <tom.debacker@vdab.be>
2022-02-02 20:16:55 +0100

    [part 2] current time rest endpoint

---
+++ src/main/java/com/realdolmen/devops/whatisthedate/date/DateController.java
@@ -0,0 +1,21 @@
+package com.realdolmen.devops.whatisthedate.date;
+
+import java.time.Clock;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DateController {
+    
+    private final Clock clock;
+    
+    public DateController(Clock clock) {
+        this.clock = clock;
+    }
+    
+    @GetMapping("/date")
+    public String getDate() {
+        return clock.instant().toString();
+    }
+    
+}
